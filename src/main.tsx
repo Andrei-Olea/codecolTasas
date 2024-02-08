cf7b328fb41bf6ba2d8cbe3c9630232b5418e396
Andrei Olea <andrei@andreiolea.com>
2024-02-08 09:55:25 -0500

    🐛 fix(main.tsx): fix import statements and add missing semicolons ✨ feat(main.tsx): remove non-null assertion from ReactDOM.createRoot method

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import Credito from './pages/Credito.tsx'
+import Credito from './pages/Credito.tsx';
 import Ahorro from './pages/Ahorro.tsx';
 import NotFoundPage from './pages/NotFoundPage.tsx';
 
-import './styles/App.scss'
+import './styles/App.scss';
 
 const router = createBrowserRouter([
   {
@@ -20,9 +20,8 @@ const router = createBrowserRouter([
   }
 ]);
 
-// biome-ignore lint/style/noNonNullAssertion: <explanation>
-ReactDOM.createRoot(document.querySelector('#root')!).render(
+ReactDOM.createRoot(document.querySelector('#root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
